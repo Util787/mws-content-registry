a6890fe0e3abd63732ce9d041b2d59c791eaa8be
Aleksandr <sanchezzz1337@gmail.com>
2025-11-29 04:25:55 +0300

    изменил methods.go и добавил комменты для читаемости

--- backend/internal/adapters/http-clients/mws-client/methods.go
+++ backend/internal/adapters/http-clients/mws-client/methods.go
@@ -1,34 +1,69 @@
 package mwsclient
 
 import (
-	"github.com/Util787/mws-content-registry/internal/models"
 	"encoding/json"
 	"fmt"
+	"log/slog"
+
+	"github.com/Util787/mws-content-registry/internal/models"
 )
 
-func (mwsClient *MWSClient) TakeAll() (Response, error) {
+// TakeAll получает все записи из MWS API
+func (mwsClient *MWSClient) TakeAll() (models.Response, error) {
+	// GET-запрос
 	res, err := mwsClient.client.R().Get(mwsClient.MWSUrl)
 	if err != nil {
-		return Response{}, err
+		mwsClient.log.Error("Failed to make request to MWS API", "error", err)
+		return models.Response{}, err
 	}
-	var response Response
 
-	err = json.Unmarshal(res.Body(), &response)
-	if err != nil {
-		return Response{}, err
+	// Парсинг JSON
+	var response models.Response
+	if err := json.Unmarshal(res.Body(), &response); err != nil {
+		mwsClient.log.Error("Failed to unmarshal MWS response", "error", err)
+		return models.Response{}, err
 	}
-	fmt.Println("////////////////////////////////////////////////////////////////////////////////")
-	fmt.Println("start")
+
+	// Логирование базовой инфы
+	mwsClient.log.Info("Fetched MWS records",
+		"total_records", len(response.Data.Records),
+		"code", response.Code,
+		"success", response.Success,
+	)
+
+	// Дополнительный вывод первых 3-х строк для отладки
 	for i, rec := range response.Data.Records {
-		fmt.Println(i)
-		fmt.Println(rec.Fields)
-		fmt.Println("////////////////////////////////////////////////////////////////////////////////")
+		if i >= 3 {
+			break
+		}
+		fmt.Println("Record:", i)
+		fmt.Println("RecordID:", rec.RecordID)
+		fmt.Println("Fields:", rec.Fields)
+		fmt.Println("----------------------------------------------------")
 	}
-	fmt.Println("end")
-	return response, err
+
+	return response, nil
 }
 
-func (mwsClient *MWSClient) TakeByID() {
-	res, err := mwsClient.client.R().Get(mwsClient.MWSUrl)
-	fmt.Println(err, res)
+// TakeByID Пример, как можно будет достать запись по ID
+func (mwsClient *MWSClient) TakeByID(id string) (models.Record, error) {
+	// Добавляем query-параметр ?id=<id>
+	url := fmt.Sprintf("%s?id=%s", mwsClient.MWSUrl, id)
+	res, err := mwsClient.client.R().Get(url)
+	if err != nil {
+		mwsClient.log.Error("Failed to fetch MWS record by ID", "id", id, "error", err)
+		return models.Record{}, err
+	}
+
+	var response models.Response
+	if err := json.Unmarshal(res.Body(), &response); err != nil {
+		mwsClient.log.Error("Failed to unmarshal MWS response for ID", "id", id, "error", err)
+		return models.Record{}, err
+	}
+
+	if len(response.Data.Records) == 0 {
+		return models.Record{}, fmt.Errorf("record with ID %s not found", id)
+	}
+
+	return response.Data.Records[0], nil
 }
